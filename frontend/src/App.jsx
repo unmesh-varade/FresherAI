1e370001f1e94c8b9b8eb6bcc66806305d9a678d
unmesh-varade <unmeshvarade.official@gmail.com>
2026-08-12 00:46:06 +0530

    feat(frontend): build home page and login interface

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {Home} from './pages/Home'
+import {Dashboard} from './pages/Dashboard'
+
+const App = () => {
+  return (
+    <>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Dashboard = () => {
+  return (
+    <div className='text-violet-700 text-3xl'>
+      Dashboard
+    </div>
+  )
+}
